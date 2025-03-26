973ce9e4828b5659e89fb669d3697dfc7aa36cca
Yadav-Varsha <varsha.yadav@waiin.com>
2025-03-26 14:09:21 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {BindingComponent} from './binding/binding.component'
+import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BindingComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-first-angular';
+  name="varsha yadav";
+  x=10;
+  y=20;
+  
+  handleClickEvent(){
+    alert("Function called")
+  }
+
 }
---
+++ src/app/binding/binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'varsha',
+  imports: [],
+  templateUrl: './binding.component.html',
+  styleUrl: './binding.component.scss'
+})
+export class BindingComponent {
+pageTitleL:string="Data Binding in Angular";
+}
